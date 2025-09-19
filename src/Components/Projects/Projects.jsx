63407634e78a39f68476bf64deed9665038ce288
Shraddha Akolkar <shraddhaaakolkar@gmail.com>
2025-09-19 10:32:06 +0530

    Uncomment project 7 data to include it in the project list

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,9 +12,9 @@ import project3 from "../../assets/project3.webp";
 import project4 from "../../assets/project4.webp";
 import project5 from "../../assets/project5.webp";
 import project6 from "../../assets/project6.webp";
-// import project7 from "../../assets/project7.webp";
+import project7 from "../../assets/project7.webp";
 import project8 from "../../assets/project8.webp";
-import project9 from "../../assets/project9.webp";
+// import project9 from "../../assets/project9.webp";
 import project10 from "../../assets/project10.webp";
 
 const Projects = () => {
@@ -55,12 +55,12 @@ const Projects = () => {
       github: "https://github.com/shraddha-akolkar/library-management-project",
       demo: "https://libraray-management-javascript.netlify.app/",
     },
-    // {
-    //   id: 7,
-    //   img: project7,
-    //   github: "https://github.com/shraddha-akolkar/real-estate",
-    //   demo: "https://estate-projects.netlify.app/",
-    // },
+    {
+      id: 7,
+      img: project7,
+      github: "https://github.com/shraddha-akolkar/real-estate",
+      demo: "https://estate-projects.netlify.app/",
+    },
 
     {
       id: 10,
@@ -75,12 +75,12 @@ const Projects = () => {
       github: "https://github.com/shraddha-akolkar/gemini-clone",
       demo: "https://gemini-clone-aii.netlify.app/",
     },
-    {
-      id: 9,
-      img: project9,
-      github: "https://github.com/shraddha-akolkar/QuickCart",
-      demo: "https://gemini-clone-aii.netlify.app/",
-    },
+    // {
+    //   id: 9,
+    //   img: project9,
+    //   github: "https://github.com/shraddha-akolkar/QuickCart",
+    //   demo: "https://gemini-clone-aii.netlify.app/",
+    // },
 
      
   ];
